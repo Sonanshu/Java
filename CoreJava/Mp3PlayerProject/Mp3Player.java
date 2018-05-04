d4ba54e1e486481d4a6dde0dc264c1b5ba36ce16
Sonanshu <sonanshugarg@gmail.com>
2018-05-04 22:20:48 +0530

    Create Mp3Player.java

---
+++ CoreJava/Mp3PlayerProject/Mp3Player.java
@@ -0,0 +1,29 @@
+package playerMp3;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import javazoom.jlme.util.Player;
+
+class Songs{
+
+    public void songplay(String songname) {
+
+        try {
+            FileInputStream fs = new FileInputStream(songname);
+            BufferedInputStream bs = new BufferedInputStream(fs);
+            Player play = new Player(bs);
+            play.play();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+public class PlayerMp3 {
+
+    public static void main(String[] args) {
+        Songs obj = new Songs();
+        obj.songplay("C:\\Users\\sonanshu\\Desktop\\Baarish.mp3");
+           
+    }
+
+}
